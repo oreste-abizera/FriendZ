9c5b9726f72b9b4045c8d6225ba30cd6eff3257e
oreste-abizera <oresteabizera11.com>
2020-07-02 11:57:03 +0200

    Latest members date

--- src/Components/DashboardPage/LatestMembers.js
+++ src/Components/DashboardPage/LatestMembers.js
@@ -36,21 +36,25 @@ export default function LatestMembers() {
       {/* <!-- /.card-header --> */}
       <div className="card-body p-0">
         <ul className="users-list clearfix">
-          {latestMembers.map((item) => (
-            <li key={item._id}>
-              <img
-                src={`${
-                  item.image ? `${url}/uploads/${item.image}` : defaultImage
-                }`}
-                alt="User"
-                style={{ minHeight: "5rem", maxHeight: "5rem" }}
-              />
-              <Link className="users-list-name" to={`/profile/${item._id}`}>
-                {item.firstName + " " + item.lastName}
-              </Link>
-              <span className="users-list-date">Today</span>
-            </li>
-          ))}
+          {latestMembers.map((item) => {
+            let date = item.createdAt.split("T")[0];
+            date = date.split("-")[2] + "/" + date.split("-")[1];
+            return (
+              <li key={item._id}>
+                <img
+                  src={`${
+                    item.image ? `${url}/uploads/${item.image}` : defaultImage
+                  }`}
+                  alt="User"
+                  style={{ minHeight: "5rem", maxHeight: "5rem" }}
+                />
+                <Link className="users-list-name" to={`/profile/${item._id}`}>
+                  {item.firstName + " " + item.lastName}
+                </Link>
+                <span className="users-list-date">{date}</span>
+              </li>
+            );
+          })}
         </ul>
         {/* <!-- /.users-list --> */}
       </div>
